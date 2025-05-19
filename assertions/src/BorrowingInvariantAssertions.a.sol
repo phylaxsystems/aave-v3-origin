f0e950b65fee286d221646519be941e21a4fec73
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2025-05-19 21:56:03 +0200

    add health factor, lending and bowrrowing invariant assertions

---
+++ assertions/src/BorrowingInvariantAssertions.a.sol
@@ -0,0 +1,258 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Assertion} from "credible-std/Assertion.sol";
+import {PhEvm} from "credible-std/PhEvm.sol";
+import {IPool} from "../../src/contracts/interfaces/IPool.sol";
+import {DataTypes} from "../../src/contracts/protocol/libraries/types/DataTypes.sol";
+import {IERC20} from "../../src/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+import {ReserveConfiguration} from "../../src/contracts/protocol/libraries/configuration/ReserveConfiguration.sol";
+import {UserConfiguration} from "../../src/contracts/protocol/libraries/configuration/UserConfiguration.sol";
+import {IVariableDebtToken} from "../../src/contracts/interfaces/IVariableDebtToken.sol";
+
+contract BorrowingPostConditionAssertions is Assertion {
+    IPool public immutable pool;
+
+    constructor(IPool _pool) {
+        pool = _pool;
+    }
+
+    function triggers() public view override {
+        // Register triggers for core borrowing functions
+        registerCallTrigger(this.assertLiabilityDecrease.selector, pool.repay.selector);
+        registerCallTrigger(this.assertUnhealthyBorrowPrevention.selector, pool.borrow.selector);
+        registerCallTrigger(this.assertFullRepayPossible.selector, pool.repay.selector);
+        registerCallTrigger(this.assertBorrowReserveState.selector, pool.borrow.selector);
+        registerCallTrigger(this.assertRepayReserveState.selector, pool.repay.selector);
+        registerCallTrigger(this.assertWithdrawNoDebt.selector, pool.withdraw.selector);
+        registerCallTrigger(this.assertBorrowCap.selector, pool.borrow.selector);
+        registerCallTrigger(this.assertBorrowBalanceChanges.selector, pool.borrow.selector);
+        registerCallTrigger(this.assertBorrowDebtChanges.selector, pool.borrow.selector);
+        registerCallTrigger(this.assertRepayBalanceChanges.selector, pool.repay.selector);
+        registerCallTrigger(this.assertRepayDebtChanges.selector, pool.repay.selector);
+    }
+
+    // BORROWING_HSPOST_A: User liability should always decrease after repayment
+    function assertLiabilityDecrease() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.repay.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (,,, address onBehalfOf) = abi.decode(callInputs[i].input, (address, uint256, uint256, address));
+
+            // Get total debt before
+            ph.forkPreState();
+            (,,, uint256 totalDebtBase,,) = pool.getUserAccountData(onBehalfOf);
+
+            // Get total debt after
+            ph.forkPostState();
+            (,,, uint256 postTotalDebtBase,,) = pool.getUserAccountData(onBehalfOf);
+
+            // Check debt decreased
+            require(postTotalDebtBase < totalDebtBase, "User liability did not decrease after repayment");
+        }
+    }
+
+    // BORROWING_HSPOST_B: Unhealthy users can not borrow
+    function assertUnhealthyBorrowPrevention() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.borrow.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (,,,, address onBehalfOf) = abi.decode(callInputs[i].input, (address, uint256, uint256, uint16, address));
+
+            // Get health factor before
+            ph.forkPreState();
+            (,,,,, uint256 healthFactor) = pool.getUserAccountData(onBehalfOf);
+
+            // If user is unhealthy, borrow should fail
+            require(healthFactor >= 1e18, "Unhealthy user was able to borrow");
+        }
+    }
+
+    // BORROWING_HSPOST_C: A user can always repay debt in full
+    function assertFullRepayPossible() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.repay.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (, uint256 amount,, address onBehalfOf) =
+                abi.decode(callInputs[i].input, (address, uint256, uint256, address));
+
+            // Get total debt before
+            ph.forkPreState();
+            (,,, uint256 totalDebtBase,,) = pool.getUserAccountData(onBehalfOf);
+
+            // Get total debt after
+            ph.forkPostState();
+            (,,, uint256 postTotalDebtBase,,) = pool.getUserAccountData(onBehalfOf);
+
+            // If amount equals total debt, debt should be 0 after
+            if (amount == totalDebtBase) {
+                require(postTotalDebtBase == 0, "Full repayment did not clear debt");
+            }
+        }
+    }
+
+    // BORROWING_HSPOST_D: An asset can only be borrowed when its configured as borrowable
+    // BORROWING_HSPOST_E: An asset can only be borrowed when the related reserve is active, not frozen, not paused & borrowing is enabled
+    function assertBorrowReserveState() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.borrow.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset,,,,) = abi.decode(callInputs[i].input, (address, uint256, uint256, uint16, address));
+
+            // Get reserve data
+            DataTypes.ReserveDataLegacy memory reserveData = pool.getReserveData(asset);
+
+            // Check reserve state
+            require(ReserveConfiguration.getActive(reserveData.configuration), "Reserve is not active");
+            require(!ReserveConfiguration.getFrozen(reserveData.configuration), "Reserve is frozen");
+            require(!ReserveConfiguration.getPaused(reserveData.configuration), "Reserve is paused");
+            require(ReserveConfiguration.getBorrowingEnabled(reserveData.configuration), "Borrowing is not enabled");
+        }
+    }
+
+    // BORROWING_HSPOST_F: An asset can only be repaid when the related reserve is active & not paused
+    function assertRepayReserveState() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.repay.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset,,,,) = abi.decode(callInputs[i].input, (address, uint256, uint256, uint16, address));
+
+            // Get reserve data
+            DataTypes.ReserveDataLegacy memory reserveData = pool.getReserveData(asset);
+
+            // Check reserve state
+            require(ReserveConfiguration.getActive(reserveData.configuration), "Reserve is not active");
+            require(!ReserveConfiguration.getPaused(reserveData.configuration), "Reserve is paused");
+        }
+    }
+
+    // BORROWING_HSPOST_G: a user should always be able to withdraw all if there is no outstanding debt
+    function assertWithdrawNoDebt() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.withdraw.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset, uint256 amount,) = abi.decode(callInputs[i].input, (address, uint256, address));
+
+            // Get total debt
+            (,,, uint256 totalDebtBase,,) = pool.getUserAccountData(callInputs[i].caller);
+
+            // If no debt, should be able to withdraw all
+            if (totalDebtBase == 0) {
+                // Get aToken balance
+                DataTypes.ReserveDataLegacy memory reserveData = pool.getReserveData(asset);
+                IERC20 aToken = IERC20(reserveData.aTokenAddress);
+                uint256 aTokenBalance = aToken.balanceOf(callInputs[i].caller);
+
+                // Should be able to withdraw all
+                require(amount <= aTokenBalance, "Cannot withdraw all when no debt");
+            }
+        }
+    }
+
+    // BORROWING_GPOST_H: If totalBorrow for a reserve increases new totalBorrow must be less than or equal to borrow cap
+    function assertBorrowCap() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.borrow.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset,,,,) = abi.decode(callInputs[i].input, (address, uint256, uint256, uint16, address));
+
+            // Get reserve data
+            DataTypes.ReserveDataLegacy memory reserveData = pool.getReserveData(asset);
+
+            // Get total borrow before
+            ph.forkPreState();
+            IVariableDebtToken variableDebtToken = IVariableDebtToken(reserveData.variableDebtTokenAddress);
+            uint256 preTotalBorrow = variableDebtToken.scaledTotalSupply();
+
+            // Get total borrow after
+            ph.forkPostState();
+            uint256 postTotalBorrow = variableDebtToken.scaledTotalSupply();
+
+            // If borrow increased, check against cap
+            require(postTotalBorrow <= preTotalBorrow, "Total borrow did not decrease");
+            uint256 borrowCap = ReserveConfiguration.getBorrowCap(reserveData.configuration);
+            if (borrowCap != 0) {
+                // 0 means no cap
+                require(postTotalBorrow <= borrowCap, "Total borrow exceeds borrow cap");
+            }
+        }
+    }
+
+    // BORROWING_HSPOST_I: After a successful borrow the actor asset balance should increase by the amount borrowed
+    function assertBorrowBalanceChanges() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.borrow.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset, uint256 amount,,,) =
+                abi.decode(callInputs[i].input, (address, uint256, uint256, uint16, address));
+
+            // Get underlying token
+            IERC20 underlying = IERC20(asset);
+
+            // Get balances before
+            ph.forkPreState();
+            uint256 preBalance = underlying.balanceOf(callInputs[i].caller);
+
+            // Get balances after
+            ph.forkPostState();
+            uint256 postBalance = underlying.balanceOf(callInputs[i].caller);
+
+            // Check balance increased by amount
+            require(postBalance - preBalance == amount, "Balance did not increase by borrow amount");
+        }
+    }
+
+    // BORROWING_HSPOST_J: After a successful borrow the onBehalf debt balance should increase by the amount borrowed
+    function assertBorrowDebtChanges() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.borrow.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (, uint256 amount,,, address onBehalfOf) =
+                abi.decode(callInputs[i].input, (address, uint256, uint256, uint16, address));
+
+            // Get debt before
+            ph.forkPreState();
+            (,,, uint256 totalDebtBase,,) = pool.getUserAccountData(onBehalfOf);
+
+            // Get debt after
+            ph.forkPostState();
+            (,,, uint256 postTotalDebtBase,,) = pool.getUserAccountData(onBehalfOf);
+
+            // Check debt increased by amount
+            require(postTotalDebtBase - totalDebtBase == amount, "Debt did not increase by borrow amount");
+        }
+    }
+
+    // BORROWING_HSPOST_K: After a successful repay the actor asset balance should decrease by the amount repaid
+    function assertRepayBalanceChanges() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.repay.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset, uint256 amount,,) = abi.decode(callInputs[i].input, (address, uint256, uint256, address));
+
+            // Get underlying token
+            IERC20 underlying = IERC20(asset);
+
+            // Get balances before
+            ph.forkPreState();
+            uint256 preBalance = underlying.balanceOf(callInputs[i].caller);
+
+            // Get balances after
+            ph.forkPostState();
+            uint256 postBalance = underlying.balanceOf(callInputs[i].caller);
+
+            // Check balance decreased by amount
+            require(preBalance - postBalance == amount, "Balance did not decrease by repay amount");
+        }
+    }
+
+    // BORROWING_HSPOST_L: After a successful repay the onBehalf debt balance should decrease by the amount repaid
+    function assertRepayDebtChanges() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.repay.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (, uint256 amount,, address onBehalfOf) =
+                abi.decode(callInputs[i].input, (address, uint256, uint256, address));
+
+            // Get debt before
+            ph.forkPreState();
+            (,,, uint256 totalDebtBase,,) = pool.getUserAccountData(onBehalfOf);
+
+            // Get debt after
+            ph.forkPostState();
+            (,,, uint256 postTotalDebtBase,,) = pool.getUserAccountData(onBehalfOf);
+
+            // Check debt decreased by amount
+            require(totalDebtBase - postTotalDebtBase == amount, "Debt did not decrease by repay amount");
+        }
+    }
+}
---
+++ assertions/src/LendingInvariantAssertions.a.sol
@@ -0,0 +1,192 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Assertion} from "credible-std/Assertion.sol";
+import {PhEvm} from "credible-std/PhEvm.sol";
+import {IPool} from "../../src/contracts/interfaces/IPool.sol";
+import {DataTypes} from "../../src/contracts/protocol/libraries/types/DataTypes.sol";
+import {IAToken} from "../../src/contracts/interfaces/IAToken.sol";
+import {IERC20} from "../../src/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+import {ReserveConfiguration} from "../../src/contracts/protocol/libraries/configuration/ReserveConfiguration.sol";
+import {UserConfiguration} from "../../src/contracts/protocol/libraries/configuration/UserConfiguration.sol";
+
+contract LendingPostConditionAssertions is Assertion {
+    IPool public immutable pool;
+
+    constructor(IPool _pool) {
+        pool = _pool;
+    }
+
+    function triggers() public view override {
+        // Register triggers for core lending functions
+        registerCallTrigger(this.assertDepositConditions.selector, pool.supply.selector);
+        registerCallTrigger(this.assertWithdrawConditions.selector, pool.withdraw.selector);
+        registerCallTrigger(this.assertTotalSupplyCap.selector, pool.supply.selector);
+        registerCallTrigger(this.assertDepositBalanceChanges.selector, pool.supply.selector);
+        registerCallTrigger(this.assertWithdrawBalanceChanges.selector, pool.withdraw.selector);
+        registerCallTrigger(this.assertCollateralWithdrawHealth.selector, pool.withdraw.selector);
+    }
+
+    // LENDING_HPOST_A: An asset can only be deposited when the related reserve is active, not frozen & not paused
+    function assertDepositConditions() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.supply.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset,,,) = abi.decode(callInputs[i].input, (address, uint256, address, uint16));
+
+            // Get reserve data
+            DataTypes.ReserveDataLegacy memory reserveData = pool.getReserveData(asset);
+
+            // Check reserve is active, not frozen and not paused
+            require(ReserveConfiguration.getActive(reserveData.configuration), "Reserve is not active");
+            require(!ReserveConfiguration.getFrozen(reserveData.configuration), "Reserve is frozen");
+            require(!ReserveConfiguration.getPaused(reserveData.configuration), "Reserve is paused");
+        }
+    }
+
+    // LENDING_HPOST_B: An asset can only be withdrawn when the related reserve is active & not paused
+    function assertWithdrawConditions() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.withdraw.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset,,) = abi.decode(callInputs[i].input, (address, uint256, address));
+
+            // Get reserve data
+            DataTypes.ReserveDataLegacy memory reserveData = pool.getReserveData(asset);
+
+            // Check reserve is active and not paused
+            require(ReserveConfiguration.getActive(reserveData.configuration), "Reserve is not active");
+            require(!ReserveConfiguration.getPaused(reserveData.configuration), "Reserve is paused");
+        }
+    }
+
+    // LENDING_GPOST_C: If totalSupply for a reserve increases new totalSupply must be less than or equal to supply cap
+    function assertTotalSupplyCap() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.supply.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset,,,) = abi.decode(callInputs[i].input, (address, uint256, address, uint16));
+
+            // Get reserve data
+            DataTypes.ReserveDataLegacy memory reserveData = pool.getReserveData(asset);
+
+            // Get aToken
+            IAToken aToken = IAToken(reserveData.aTokenAddress);
+
+            // Get total supply before and after
+            ph.forkPreState();
+            uint256 preTotalSupply = aToken.totalSupply();
+
+            ph.forkPostState();
+            uint256 postTotalSupply = aToken.totalSupply();
+
+            // If supply increased, check against cap
+            require(postTotalSupply > preTotalSupply, "Total supply did not increase");
+            uint256 supplyCap = ReserveConfiguration.getSupplyCap(reserveData.configuration);
+            if (supplyCap != 0) {
+                // 0 means no cap
+                require(postTotalSupply <= supplyCap, "Total supply exceeds supply cap");
+            }
+        }
+    }
+
+    // LENDING_HPOST_D: After a successful deposit the sender underlying balance should decrease by the amount deposited
+    // LENDING_HPOST_E: After a successful deposit the onBehalf AToken balance should increase by the amount deposited
+    function assertDepositBalanceChanges() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.supply.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset, uint256 amount, address onBehalfOf,) =
+                abi.decode(callInputs[i].input, (address, uint256, address, uint16));
+
+            // Get reserve data
+            DataTypes.ReserveDataLegacy memory reserveData = pool.getReserveData(asset);
+
+            // Get aToken
+            IAToken aToken = IAToken(reserveData.aTokenAddress);
+
+            // Get underlying token
+            IERC20 underlying = IERC20(asset);
+
+            // Get balances before
+            ph.forkPreState();
+            uint256 preSenderBalance = underlying.balanceOf(callInputs[i].caller);
+            uint256 preATokenBalance = aToken.balanceOf(onBehalfOf);
+
+            // Get balances after
+            ph.forkPostState();
+            uint256 postSenderBalance = underlying.balanceOf(callInputs[i].caller);
+            uint256 postATokenBalance = aToken.balanceOf(onBehalfOf);
+
+            // Check sender balance decreased by amount
+            require(preSenderBalance - postSenderBalance == amount, "Sender balance did not decrease by deposit amount");
+
+            // Check aToken balance increased by amount
+            require(postATokenBalance - preATokenBalance == amount, "AToken balance did not increase by deposit amount");
+        }
+    }
+
+    // LENDING_HPOST_F: After a successful withdraw the actor AToken balance should decrease by the amount withdrawn
+    // LENDING_HPOST_G: After a successful withdraw the `to` underlying balance should increase by the amount withdrawn
+    function assertWithdrawBalanceChanges() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.withdraw.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset, uint256 amount, address to) = abi.decode(callInputs[i].input, (address, uint256, address));
+
+            // Get reserve data
+            DataTypes.ReserveDataLegacy memory reserveData = pool.getReserveData(asset);
+
+            // Get aToken
+            IAToken aToken = IAToken(reserveData.aTokenAddress);
+
+            // Get underlying token
+            IERC20 underlying = IERC20(asset);
+
+            // Get balances before
+            ph.forkPreState();
+            uint256 preATokenBalance = aToken.balanceOf(callInputs[i].caller);
+            uint256 preToBalance = underlying.balanceOf(to);
+
+            // Get balances after
+            ph.forkPostState();
+            uint256 postATokenBalance = aToken.balanceOf(callInputs[i].caller);
+            uint256 postToBalance = underlying.balanceOf(to);
+
+            // Check aToken balance decreased by amount
+            require(
+                preATokenBalance - postATokenBalance == amount, "AToken balance did not decrease by withdraw amount"
+            );
+
+            // Check underlying balance increased by amount
+            require(postToBalance - preToBalance == amount, "Underlying balance did not increase by withdraw amount");
+        }
+    }
+
+    // LENDING_HPOST_H1: Before a successful withdraw of collateral, caller should be healthy
+    // LENDING_HPOST_H2: After a successful withdraw of collateral, caller should remain healthy
+    function assertCollateralWithdrawHealth() external {
+        PhEvm.CallInputs[] memory callInputs = ph.getCallInputs(address(pool), pool.withdraw.selector);
+        for (uint256 i = 0; i < callInputs.length; i++) {
+            (address asset,,) = abi.decode(callInputs[i].input, (address, uint256, address));
+
+            // Get reserve data
+            DataTypes.ReserveDataLegacy memory reserveData = pool.getReserveData(asset);
+
+            // Check if asset is being used as collateral
+            DataTypes.UserConfigurationMap memory userConfig = pool.getUserConfiguration(callInputs[i].caller);
+            bool isCollateral = UserConfiguration.isUsingAsCollateral(userConfig, reserveData.id);
+
+            if (isCollateral) {
+                // Get health factor before
+                ph.forkPreState();
+                (,,,,, uint256 preHealthFactor) = pool.getUserAccountData(callInputs[i].caller);
+
+                // Check health before withdraw
+                require(preHealthFactor >= 1e18, "User not healthy before collateral withdraw");
+
+                // Get health factor after
+                ph.forkPostState();
+                (,,,,, uint256 postHealthFactor) = pool.getUserAccountData(callInputs[i].caller);
+
+                // Check health after withdraw
+                require(postHealthFactor >= 1e18, "User not healthy after collateral withdraw");
+            }
+        }
+    }
+}
